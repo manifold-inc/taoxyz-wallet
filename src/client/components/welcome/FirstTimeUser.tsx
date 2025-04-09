ba006ce29ea7acbe9f9d535921530800f59cd206
Alan Cho <alancho397@gmail.com>
2025-04-09 11:44:40 -0500

    fix: add prettier config

--- src/client/components/welcome/FirstTimeUser.tsx
+++ src/client/components/welcome/FirstTimeUser.tsx
@@ -1,5 +1,14 @@
+import taoxyz from '@public/icons/taoxyz.svg';
+
 const FirstTimeUser = () => {
-  return <div>FirstTimeUser</div>;
+  return (
+    <div className="bg-mf-safety-500 flex h-screen w-screen items-center justify-center">
+      <div className="bg-mf-milk-500 rounded-lg p-4">
+        <img src={taoxyz} alt="Taoxyz Logo" className="h-16 w-16" />
+        <h1>FirstTimeUser</h1>
+      </div>
+    </div>
+  );
 };
 
 export default FirstTimeUser;
