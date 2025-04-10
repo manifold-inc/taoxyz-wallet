4eba0c8a199e0694916f67351b6fe34fdbd4898c
Alan Cho <alancho397@gmail.com>
2025-04-10 01:41:45 -0500

    style: add animation for logo and text

--- src/client/components/welcome/FirstTimeUser.tsx
+++ src/client/components/welcome/FirstTimeUser.tsx
@@ -8,11 +8,14 @@ const FirstTimeUser = () => {
         <div className="absolute inset-0 bg-mf-safety-500 scale-down-center z-10" />
 
         {/* Logo Animation Container */}
-        <div className="flex items-center justify-center z-10 slide-left">
+        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-10 slide-left">
           <img src={taoxyz} alt="Taoxyz Logo" className="h-8 w-8" />
         </div>
+        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-10 slide-right">
+          <p className="text-mf-edge-500 text-4xl font-bold font-blinker">TAO.XYZ</p>
+        </div>
 
-        <div className="absolute bottom-20 px-10 py-1.5 bg-mf-ash-500 rounded-full z-0">
+        <div className="absolute bottom-20 px-10 py-1.5 bg-mf-ash-500 rounded-full z-0 slide-up">
           <button className="text-mf-sybil-500 text-lg rounded-full cursor-pointer">
             Get Started
           </button>
