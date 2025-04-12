456379eed96fdd98884a7b17f41f33535131eaab
Alan Cho <alancho397@gmail.com>
2025-04-12 15:42:12 -0500

    ref: abstract out overview into ocmponent

--- src/client/components/dashboard/DashboardOverview.tsx
+++ src/client/components/dashboard/DashboardOverview.tsx
@@ -1,159 +1,115 @@
-// /**
-//  * Total tao
-//  * Free tao
-//  * Equivalent in USD
-//  * Price of tao
-//  * percentage change of tao from 24hr ago
-//  * Pie chart showing stake distribution
-//  *
-//  */
-// import { useRef, useState } from 'react';
+import GreenDollar from '@public/assets/green-dollar.svg';
+import SilverDollar from '@public/assets/silver-dollar.svg';
+import { Copy } from 'lucide-react';
 
-// import { useNotification } from '@/client/contexts/NotificationContext';
-// import { usePolkadotApi } from '@/client/contexts/PolkadotApiContext';
-// import { useWallet } from '@/client/contexts/WalletContext';
-// import { NotificationType, type Subnet } from '@/types/client';
-// import { formatNumber, raoToTao } from '@/utils/utils';
+import { useEffect, useState } from 'react';
 
-// interface StakeResponse {
-//   netuid: number;
-//   hotkey: string;
-//   stake: number;
-// }
+import { useNotification } from '@/client/contexts/NotificationContext';
+import { useWallet } from '@/client/contexts/WalletContext';
+import { NotificationType, type Stake, type Subnet } from '@/types/client';
+import { formatNumber, raoToTao } from '@/utils/utils';
 
-// const DashboardOverview = ({ stakes }: { stakes: any }) => {
-//   const { api } = usePolkadotApi();
-//   const { showNotification } = useNotification();
-//   const { currentAddress } = useWallet();
-//   const [totalTao, setTotalTao] = useState<number>(0);
-//   const [freeTao, setFreeTao] = useState<number>(0);
-//   const [totalTaoUSD, setTotalTaoUSD] = useState<number>(0);
-//   const [freeTaoUSD, setFreeTaoUSD] = useState<number>(0);
+interface DashboardOverviewProps {
+  stakes: Stake[];
+  subnets: Subnet[];
+  freeTao: number;
+  taoPrice: number | null;
+  priceChangePercentage: number | null;
+  isLoading: boolean;
+}
 
-//   const [isLoading, setIsLoading] = useState<boolean>(false);
-//   const prevFetchRef = useRef<string | null>(null);
+const DashboardOverview = ({
+  stakes,
+  subnets,
+  freeTao = 0,
+  taoPrice,
+  priceChangePercentage,
+  isLoading = true,
+}: DashboardOverviewProps) => {
+  const { currentAddress } = useWallet();
+  const { showNotification } = useNotification();
+  const [totalTao, setTotalTao] = useState<number | null>(null);
+  const [copied, setCopied] = useState(false);
 
-//   const fetchData = async (address: string, forceRefresh = false): Promise<void> => {
-//     if (!api || !address || (!forceRefresh && address === prevFetchRef.current)) return;
-//     setIsLoading(true);
-//     prevFetchRef.current = address;
+  const calculateTotalTao = (): void => {
+    let total = freeTao;
+    for (const stake of stakes) {
+      const subnet = subnets.find(subnet => subnet.id === stake.netuid) as Subnet;
+      if (subnet) {
+        total += raoToTao(BigInt(stake.stake)) * subnet.price;
+      }
+    }
 
-//     try {
-//       const [subnetsResult, freeBalanceResult, stakeResult] = await Promise.all([
-//         api.getSubnets(),
-//         api.getBalance(address),
-//         api.getStake(address),
-//       ]);
+    console.log('Overview Data', { total, freeTao, stakes, subnets });
+    setTotalTao(total);
+  };
 
-//       if (subnetsResult === null) {
-//         showNotification({
-//           type: NotificationType.Error,
-//           message: 'Failed to Fetch Subnets',
-//         });
-//         return;
-//       }
+  const handleCopy = async (): Promise<void> => {
+    if (!currentAddress) return;
+    await navigator.clipboard.writeText(currentAddress);
+    setCopied(true);
+    setTimeout(() => setCopied(false), 2000);
+    showNotification({
+      type: NotificationType.Success,
+      message: 'Address Copied',
+    });
+  };
 
-//       if (freeBalanceResult === null) {
-//         showNotification({
-//           type: NotificationType.Error,
-//           message: 'Failed to Fetch Balance',
-//         });
-//         return;
-//       }
+  useEffect(() => {
+    void calculateTotalTao();
+  }, [currentAddress, stakes, subnets, freeTao]);
 
-//       if (stakeResult === null) {
-//         showNotification({
-//           type: NotificationType.Error,
-//           message: 'Failed to Fetch Stakes',
-//         });
-//         return;
-//       }
+  if (isLoading) {
+    console.log('isLoading');
+  }
 
-//       const formattedStakes = await Promise.all(
-//         (stakeResult as unknown as StakeResponse[]).map(async stake => {
-//           const subnet = await api.getSubnet(stake.netuid);
-//           return {
-//             subnetId: stake.netuid,
-//             subnetName: subnet?.name ?? `Subnet ${stake.netuid}`,
-//             validatorHotkey: stake.hotkey,
-//             tokens: stake.stake,
-//           };
-//         })
-//       );
+  return (
+    <div className="w-full h-full rounded-sm bg-mf-sybil-opacity p-2 flex justify-between">
+      {/* Total and Free TAO */}
+      <div className="flex flex-col items-center justify-center gap-2">
+        <div className="flex flex-col items-center justify-center">
+          <div className="flex items-center justify-center gap-0.5">
+            <img src={SilverDollar} alt="Silver Dollar" className="w-4 h-4" />
+            <p className="text-mf-edge-500 font-semibold text-xl">{formatNumber(totalTao ?? 0)}</p>
+          </div>
+          <p className="text-mf-edge-500 text-xs">Total Tao</p>
+        </div>
 
-//       let totalBalance = freeBalanceResult;
-//       for (const stake of formattedStakes) {
-//         const subnet = subnetsResult.find(subnet => subnet.id === stake.subnetId) as Subnet;
-//         if (subnet) {
-//           totalBalance += raoToTao(BigInt(stake.tokens)) * subnet.price;
-//         }
-//       }
+        <div className="flex flex-col items-center justify-center">
+          <div className="flex items-center justify-center gap-0.5">
+            <img src={GreenDollar} alt="Green Dollar" className="w-4 h-4" />
+            <p className="text-mf-sybil-500 font-semibold text-xl">{formatNumber(freeTao ?? 0)}</p>
+          </div>
+          <p className="text-mf-sybil-500 text-xs">Free Tao</p>
+        </div>
+      </div>
 
-//       setTotalBalance(totalBalance);
-//       setBalance(balanceResult);
-//       setStakes(formattedStakes);
-//     } finally {
-//       setIsLoading(false);
-//     }
-//   };
+      {/* Address, TAO Price, TAO Percentage Change */}
+      <div className="flex flex-col items-center justify-center gap-2">
+        <div className="flex items-center justify-center gap-1">
+          <p className="text-mf-sybil-500 text-xs">
+            {currentAddress?.slice(0, 4)}...{currentAddress?.slice(-4)}
+          </p>
+          <button onClick={handleCopy} className="cursor-pointer bg-mf-ash-500 rounded-full p-1.5">
+            <Copy
+              className={`w-3 h-3 cursor-pointer ${
+                copied ? 'text-mf-edge-500' : 'text-mf-sybil-500'
+              }`}
+            />
+          </button>
+        </div>
 
-//   const fetchUSDToTao = async (): Promise<void> => {
-//     setIsLoading(true);
-//     try {
-//       const response = await fetch(
-//         'https://api.coingecko.com/api/v3/coins/bittensor/market_chart?vs_currency=usd&days=1',
-//         {
-//           method: 'GET',
-//           headers: {
-//             'Content-Type': 'application/json',
-//           },
-//         }
-//       );
-//       const data = await response.json();
-//       const prices = data.prices;
-//       const recentPrice = prices[0][1];
-//       const dayOldPrice = prices[prices.length - 1][1];
-//       setUsdToTao(recentPrice);
-//       setDayOldUsdToTao(dayOldPrice);
-//     } catch {
-//       showNotification({
-//         type: NotificationType.Error,
-//         message: 'Failed to Fetch USD to TAO Price',
-//       });
-//     } finally {
-//       setIsLoading(false);
-//     }
-//   };
+        <div className="flex flex-col items-center justify-center gap-1">
+          <p
+            className={`text-xs ${priceChangePercentage && priceChangePercentage >= 0 ? 'text-mf-sybil-500' : 'text-mf-safety-500'}`}
+          >
+            {priceChangePercentage?.toFixed(2)}%
+          </p>
+          <p className="text-mf-edge-500 text-xs">${taoPrice?.toFixed(2)}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-//   const handleCopy = async (): Promise<void> => {
-//     if (!currentAddress) return;
-//     await navigator.clipboard.writeText(currentAddress);
-//     setCopied(true);
-//     setTimeout(() => setCopied(false), 2000);
-//     showNotification({
-//       type: NotificationType.Success,
-//       message: 'Address Copied',
-//     });
-//   };
-
-//   if (api && currentAddress && currentAddress !== prevFetchRef.current) {
-//     void fetchData(currentAddress);
-//     void fetchUSDToTao();
-//   }
-
-//   return (
-//     <div className="w-full h-full rounded-sm bg-mf-sybil-opacity p-2">
-//       {/* Total and Free TAO */}
-//       <div className="flex flex-col items-center justify-center gap-2">
-//         <div>
-//           <span>{formatNumber(totalTao)}</span>
-//         </div>
-//         <div>
-//           <span>{formatNumber(freeTao)}</span>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default DashboardOverview;
+export default DashboardOverview;
--- src/client/pages/Dashboard.tsx
+++ src/client/pages/Dashboard.tsx
@@ -1,53 +1,64 @@
-import { Copy } from 'lucide-react';
-
 import { useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import clock from '../../../public/assets/clock.svg';
-import dollar from '../../../public/assets/dollar.svg';
-import taoxyz from '../../../public/icons/taoxyz.png';
-import type { StakeTransaction, Subnet } from '../../types/client';
+import type { Stake, Subnet } from '../../types/client';
 import { NotificationType } from '../../types/client';
-import { formatNumber, raoToTao } from '../../utils/utils';
 import WalletSelection from '../components/common/WalletSelection';
-import Portfolio from '../components/dashboard/Portfolio';
+import DashboardOverview from '../components/dashboard/DashboardOverview';
+// import Portfolio from '../components/dashboard/Portfolio';
 import { useNotification } from '../contexts/NotificationContext';
 import { usePolkadotApi } from '../contexts/PolkadotApiContext';
 import { useWallet } from '../contexts/WalletContext';
 
-interface StakeResponse {
-  netuid: number;
-  hotkey: string;
-  stake: number;
+const API_URL = 'https://api.coingecko.com/api/v3';
+const NETWORK_ID = 'bittensor';
+
+// [Timestamp, Price]
+interface PriceData {
+  prices: [number, number][];
 }
+
+const calculatePriceChange = (
+  prices: [number, number][]
+): { price: number; priceChange: number } => {
+  const [price, oldPrice] = [prices[0][1], prices[prices.length - 1][1]];
+  const priceChange = ((price - oldPrice) / oldPrice) * 100;
+
+  return {
+    price,
+    priceChange,
+  };
+};
 
 export const Dashboard = () => {
   const navigate = useNavigate();
   const { showNotification } = useNotification();
   const { api } = usePolkadotApi();
   const { currentAddress } = useWallet();
-  const [isLoading, setIsLoading] = useState(true);
-  const [balance, setBalance] = useState<number | null>(null);
-  const [totalBalance, setTotalBalance] = useState<number | null>(null);
-  const [stakes, setStakes] = useState<StakeTransaction[]>([]);
-  const [copied, setCopied] = useState(false);
-  const [usdToTao, setUsdToTao] = useState<number | null>(null);
-  const [dayOldUsdToTao, setDayOldUsdToTao] = useState<number | null>(null);
-  const prevFetchRef = useRef<string | null>(null);
+
+  const [stakes, setStakes] = useState<Stake[]>([]);
+  const [subnets, setSubnets] = useState<Subnet[]>([]);
+  const [freeTao, setFreeTao] = useState<number | null>(null);
+
+  const [taoPrice, setTaoPrice] = useState<number | null>(null);
+  const [priceChangePercentage, setPriceChangePercentage] = useState<number | null>(null);
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const prevAddressRef = useRef<string | null>(null);
 
   const fetchData = async (address: string, forceRefresh = false): Promise<void> => {
-    if (!api || !address || (!forceRefresh && address === prevFetchRef.current)) return;
+    if (!api || !address || (!forceRefresh && address === prevAddressRef.current)) return;
     setIsLoading(true);
-    prevFetchRef.current = address;
+    prevAddressRef.current = address;
 
     try {
-      const [subnetsResult, balanceResult, stakeResult] = await Promise.all([
+      const [subnets, freeTao, stakes] = await Promise.all([
         api.getSubnets(),
         api.getBalance(address),
         api.getStake(address),
       ]);
 
-      if (!subnetsResult) {
+      if (subnets === null) {
         showNotification({
           type: NotificationType.Error,
           message: 'Failed to Fetch Subnets',
@@ -55,15 +66,15 @@ export const Dashboard = () => {
         return;
       }
 
-      if (balanceResult === null) {
+      if (freeTao === null) {
         showNotification({
           type: NotificationType.Error,
-          message: 'Failed to Fetch Balance',
+          message: 'Failed to Fetch Free TAO',
         });
         return;
       }
 
-      if (!stakeResult) {
+      if (stakes === null) {
         showNotification({
           type: NotificationType.Error,
           message: 'Failed to Fetch Stakes',
@@ -71,197 +82,78 @@ export const Dashboard = () => {
         return;
       }
 
-      const formattedStakes = await Promise.all(
-        (stakeResult as unknown as StakeResponse[]).map(async stake => {
-          const subnet = await api.getSubnet(stake.netuid);
-          return {
-            subnetId: stake.netuid,
-            subnetName: subnet?.name ?? `Subnet ${stake.netuid}`,
-            validatorHotkey: stake.hotkey,
-            tokens: stake.stake,
-          };
-        })
-      );
-
-      console.log('stakes: ', stakes);
-      let totalBalance = balanceResult;
-
-      for (const stake of formattedStakes) {
-        const subnet = subnetsResult.find(subnet => subnet.id === stake.subnetId) as Subnet;
-
-        if (subnet) {
-          totalBalance += raoToTao(BigInt(stake.tokens)) * subnet.price;
-        }
-      }
-
-      console.log('formatted stakes: ', formattedStakes);
-
-      setTotalBalance(totalBalance);
-      setBalance(balanceResult);
-      setStakes(formattedStakes);
+      setSubnets(subnets);
+      setFreeTao(freeTao);
+      setStakes(stakes);
+      console.log('Dashboard Data', { subnets, freeTao, stakes });
     } finally {
       setIsLoading(false);
     }
   };
 
-  const fetchUSDToTao = async (): Promise<void> => {
+  const fetchTaoPrice = async (): Promise<void> => {
     setIsLoading(true);
     try {
       const response = await fetch(
-        'https://api.coingecko.com/api/v3/coins/bittensor/market_chart?vs_currency=usd&days=1',
-        {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
+        `${API_URL}/coins/${NETWORK_ID}/market_chart?vs_currency=usd&days=1`
       );
-      const data = await response.json();
-      const prices = data.prices;
-      const recentPrice = prices[0][1];
-      const dayOldPrice = prices[prices.length - 1][1];
-      setUsdToTao(recentPrice);
-      setDayOldUsdToTao(dayOldPrice);
+      const data = (await response.json()) as PriceData;
+      const { price, priceChange } = calculatePriceChange(data.prices);
+      setTaoPrice(price);
+      setPriceChangePercentage(priceChange);
     } catch {
       showNotification({
         type: NotificationType.Error,
-        message: 'Failed to Fetch USD to TAO Price',
+        message: 'Failed to Fetch TAO Price',
       });
     } finally {
       setIsLoading(false);
     }
   };
 
-  const handleCopy = async (): Promise<void> => {
-    if (!currentAddress) return;
-    await navigator.clipboard.writeText(currentAddress);
-    setCopied(true);
-    setTimeout(() => setCopied(false), 2000);
-    showNotification({
-      type: NotificationType.Success,
-      message: 'Address Copied',
-    });
-  };
-
-  if (api && currentAddress && currentAddress !== prevFetchRef.current) {
+  if (api && currentAddress && currentAddress !== prevAddressRef.current) {
     void fetchData(currentAddress);
-    void fetchUSDToTao();
+    void fetchTaoPrice();
   }
 
   return (
-    <div className="flex flex-col items-center w-full h-full pt-16">
+    <div className="flex flex-col items-center w-full h-full pt-16 bg-mf-night-500 px-5">
       {/* Wallet Selection */}
       <WalletSelection />
 
-      <div className="mt-2 border-sm border-2 border-mf-ash-500 bg-mf-ash-500 flex p-1 justify-center items-center gap-4">
-        {/* Price of TAO */}
-        <div className="flex items-center gap-1">
-          <img src={dollar} alt="Dollar Logo" className="w-4 h-4" />
-          <span className="text-base font-semibold text-mf-edge-500">
-            {usdToTao ? `${formatNumber(usdToTao).toFixed(2)}` : 'Loading...'}
-          </span>
-          {usdToTao && <span className="text-[10px] ml-0.5 text-mf-edge-500">/τ</span>}
-        </div>
-
-        {/* Price of TAO 24hr ago */}
-        <div className="flex items-center gap-1">
-          <img src={clock} alt="Clock Logo" className="w-4 h-4" />
-          <span className="text-base font-semibold text-mf-edge-500">
-            {dayOldUsdToTao ? `${formatNumber(dayOldUsdToTao).toFixed(2)}` : 'Loading...'}
-          </span>
-          {dayOldUsdToTao && <span className="text-[10px] ml-0.5 text-mf-edge-500">24hr</span>}
-        </div>
-      </div>
-
-      <div className="mt-2 border-sm border-2 border-mf-ash-500 bg-mf-ash-500 gap-2">
-        {/* Address & Account Value */}
-        <div className="border-b border-mf-ash-300">
-          <div className="flex items-center justify-between py-1 px-2">
-            {/* Address */}
-            <div className="flex items-center text-xs text-mf-milk-300 gap-1">
-              <p>
-                {!currentAddress
-                  ? 'Loading...'
-                  : `${currentAddress.slice(0, 4)}...${currentAddress.slice(-4)}`}
-              </p>
-              <button onClick={() => void handleCopy()} className="transition-colors">
-                {currentAddress && (
-                  <Copy
-                    className={`w-3 h-3 cursor-pointer ${
-                      copied ? 'text-mf-sybil-500' : 'text-mf-milk-300'
-                    }`}
-                  />
-                )}
-              </button>
-            </div>
-
-            {/* Account Value */}
-            <div className="flex items-center text-xs font-semibold text-mf-sybil-500 space-x-1">
-              <p>
-                {usdToTao === null || totalBalance === null
-                  ? 'Loading...'
-                  : `$${formatNumber(Number(usdToTao * totalBalance)).toFixed(2)}`}
-              </p>
-            </div>
-          </div>
-        </div>
-
-        {/* Balance */}
-        <div className="px-2 py-1">
-          {/* Total Balance */}
-          <div className="flex items-center gap-2">
-            <img src={taoxyz} alt="Taoxyz Logo" className="w-3 h-3" />
-            <span className="text-xl text-mf-milk-300 font-semibold">
-              {totalBalance === null
-                ? 'Loading'
-                : Number(totalBalance) === 0
-                  ? '0'
-                  : formatNumber(Number(totalBalance))}
-            </span>
-            <span className="text-xs text-mf-edge-300">Total</span>
-          </div>
-
-          {/* Free Balance */}
-          <div className="flex items-center gap-2">
-            <img src={taoxyz} alt="Taoxyz Logo" className="w-3 h-3" />
-            <span className="text-sm text-mf-sybil-500 font-semibold">
-              {balance === null
-                ? 'Loading'
-                : Number(balance) === 0
-                  ? '0'
-                  : formatNumber(Number(balance))}
-            </span>
-            <span className="text-xs text-mf-sybil-500">Free</span>
-          </div>
-        </div>
-      </div>
+      <DashboardOverview
+        stakes={stakes}
+        subnets={subnets}
+        freeTao={freeTao ?? 0}
+        taoPrice={taoPrice}
+        priceChangePercentage={priceChangePercentage}
+        isLoading={isLoading}
+      />
 
       {/* Actions */}
-      <div className="mt-3">
-        <div className="flex justify-between rounded-sm text-sm text-mf-night-500 transition-colors space-x-2">
-          <button
-            onClick={() => navigate('/add-stake')}
-            className="w-1/3 p-1 bg-mf-safety-500 hover:bg-mf-night-500 hover:text-mf-safety-500 border-2 border-mf-safety-500 hover:border-mf-safety-500 border-sm transition-colors cursor-pointer"
-          >
-            <span>Add</span>
-          </button>
-          <button
-            onClick={() => navigate('/move-stake')}
-            className="w-1/3 p-1 bg-mf-safety-500 hover:bg-mf-night-500 hover:text-mf-safety-500 border-2 border-mf-safety-500 hover:border-mf-safety-500 border-sm transition-colors cursor-pointer"
-          >
-            <span>Move</span>
-          </button>
-          <button
-            onClick={() => navigate('/transfer')}
-            className="w-1/3 p-1 bg-mf-sybil-500 hover:bg-mf-night-500 hover:text-mf-sybil-500 border-2 border-mf-sybil-500 hover:border-mf-sybil-500 border-sm transition-colors cursor-pointer"
-          >
-            <span>Transfer</span>
-          </button>
-        </div>
+      <div className="flex justify-between rounded-sm text-sm text-mf-night-500 transition-colors gap-2 pt-3">
+        <button
+          onClick={() => navigate('/add-stake')}
+          className="w-1/3 p-1 bg-mf-safety-500 hover:bg-mf-night-500 hover:text-mf-safety-500 border-2 border-mf-safety-500 hover:border-mf-safety-500 border-sm transition-colors cursor-pointer"
+        >
+          <span>Add</span>
+        </button>
+        <button
+          onClick={() => navigate('/move-stake')}
+          className="w-1/3 p-1 bg-mf-safety-500 hover:bg-mf-night-500 hover:text-mf-safety-500 border-2 border-mf-safety-500 hover:border-mf-safety-500 border-sm transition-colors cursor-pointer"
+        >
+          <span>Move</span>
+        </button>
+        <button
+          onClick={() => navigate('/transfer')}
+          className="w-1/3 p-1 bg-mf-sybil-500 hover:bg-mf-night-500 hover:text-mf-sybil-500 border-2 border-mf-sybil-500 hover:border-mf-sybil-500 border-sm transition-colors cursor-pointer"
+        >
+          <span>Transfer</span>
+        </button>
       </div>
 
       {/* Portfolio Overview */}
-      <div className="mt-3">
+      {/* <div className="mt-3">
         <h2 className="text-xs text-mf-sybil-500 font-semibold">Portfolio</h2>
         {isLoading ? (
           <div className="border-sm border-2 border-mf-ash-500 p-2 bg-mf-ash-500 text-sm text-mf-milk-300">
@@ -274,7 +166,7 @@ export const Dashboard = () => {
             onRefresh={() => (currentAddress ? fetchData(currentAddress, true) : Promise.resolve())}
           />
         )}
-      </div>
+      </div> */}
     </div>
   );
 };
