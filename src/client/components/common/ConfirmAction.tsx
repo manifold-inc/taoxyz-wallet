68b9179d3cad7f338be910e389d6a7e556722fa7
Alan Cho <alancho397@gmail.com>
2025-04-23 00:41:30 -0500

    style: clean up confirm action

--- src/client/components/common/ConfirmAction.tsx
+++ src/client/components/common/ConfirmAction.tsx
@@ -18,23 +18,23 @@ const ConfirmAction = ({ isOpen, title, message, onConfirm, onCancel }: ConfirmA
         event.stopPropagation();
       }}
     >
-      <div className="relative z-[100] bg-mf-night-500 border-sm border-2 border-mf-ash-500 p-6 max-w-md w-full mx-6">
+      <div className="relative z-[100] bg-mf-ash-500 rounded-md p-6 max-w-md w-full mx-6">
         <div className="flex w-full flex-col items-center justify-center space-y-6">
           <div className="flex w-full flex-col items-center space-y-4">
-            <h2 className="text-mf-milk-300 text-base">{title}</h2>
-            <p className="text-mf-safety-500 text-xs">{message}</p>
+            <h2 className="text-mf-edge-500 text-base font-medium">{title}</h2>
+            <p className="text-mf-safety-500 text-xs font-light">{message}</p>
           </div>
 
           <div className="flex w-full items-center space-x-2">
             <button
               onClick={onCancel}
-              className="p-2 flex-1 text-xs text-mf-safety-500 border-2 border-sm border-mf-safety-500 bg-mf-night-500 hover:bg-mf-safety-500 hover:text-mf-night-500"
+              className="p-2 flex-1 text-xs rounded-md font-medium text-mf-safety-500 bg-mf-ash-500 hover:opacity-50"
             >
               Cancel
             </button>
             <button
               onClick={onConfirm}
-              className="p-2 flex-1 text-xs text-mf-night-500 border-2 border-sm border-mf-safety-500 bg-mf-safety-500 hover:bg-mf-night-500 hover:text-mf-safety-500"
+              className="p-2 flex-1 text-xs rounded-md font-medium text-mf-night-500 bg-mf-sybil-500 hover:opacity-50"
             >
               Confirm
             </button>
